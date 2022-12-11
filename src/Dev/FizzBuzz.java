7dd921c4c64c09664f522bc13e4ef5003373ad7c
fabiokenda <fabio_kng@gmx.de>
2022-12-11 15:18:36 +0100

    #

---
+++ src/Dev/FizzBuzz.java
@@ -0,0 +1,16 @@
+package Dev;
+
+public class FizzBuzz {
+
+    public String convertNumber(int number) {
+        if(number % 5 == 0 && number % 7 == 0) {
+            return "FizzBuzz";
+        }
+        if(number % 5 == 0) {
+            return "Fizz";
+        } else if(number % 7 == 0) {
+            return "Buzz";
+        }
+        return "Hello World!";
+    }
+}
